6626d1805a2f07b76a8e086626c872c705e1efc9
Aramuni <39601135+Aramunii@users.noreply.github.com>
2022-11-11 17:36:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 
+process.on('uncaughtException', (err,origin) => {
+    console.log(`Uncaught Exception: ${err.message}`)
+})
 
 app.use(
     cors({
@@ -11,21 +14,30 @@ app.use(
         origin: true
     })
 );
+
+app.use((error, req, res, next) => {
+    console.log('error middleware');
+    res.sendStatus(500);
+})
+
 app.use(express.urlencoded({ extended: true }))
 
 app.options('*', cors());
 
-app.get('', async (req, res) => {
-res.send('oi');
-})
-
 app.get('/search', async (req, res) => {
     search = req.query.q;
     var file = { url: search };
-    html_to_pdf.generatePdf(file, options).then(pdfBuffer => {       
-        res.send(pdfBuffer)
-    });
+    try {
+        html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+            res.send(pdfBuffer)
+        });
+    } catch (e) {
+        console.log('aaa');
+        res.send('error')
+    }
+
 });
+
 
 
 app.listen(process.env.PORT || 30850, function () {
