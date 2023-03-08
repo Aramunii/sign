d36fcf1d69b75451166551b5daf78361ec3b0e8f
Aramuni <39601135+Aramunii@users.noreply.github.com>
2023-03-08 16:12:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,28 +26,17 @@ app.options('*', cors());
 
 app.get('/search', async (req, res) => {
     search = req.query.q;
+    console.log(search);
     var file = { url: search };
     try {
         html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
             res.send(pdfBuffer)
         });
     } catch (e) {
-        console.log('aaa');
         res.send('error')
     }
-
 });
-
-
 
 app.listen(process.env.PORT || 30850, function () {
     console.log('server running on port 30850', '');
 });
-
-
-let options = { format: 'A4' };
-// Example of options with args //
-// let options = { format: 'A4', args: ['--no-sandbox', '--disable-setuid-sandbox'] };
-
-// var file = { content: "<h1>Welcome to html-pdf-node</h1>" };
-// or //
